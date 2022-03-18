ace26fdbb6b3b61399f5a259eb2afbdb48cb5e92
ayb <ayb@stingm.home>
2022-03-18 14:33:08 +0100

    primer commit

--- first_code.tf
+++ first_code.tf
@@ -3,7 +3,7 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource " aws_s3_bucket" "tf_course" {
+resource "aws_s3_bucket" "tf_course" {
   bucket = "tf-course-ln-20220318"
   acl    = "private"
 }
