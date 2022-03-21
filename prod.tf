9bd74bf0f9a97022fe33fb2ba796232217ea209e
alelova <alelova@gmail.com>
2022-03-21 16:28:44 +0100

    addopt default vpc

--- prod.tf
+++ prod.tf
@@ -3,6 +3,9 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_s3_bucket" "tf_course" {
+resource "aws_s3_bucket" "prod_tf_course" {
   bucket = "tf-course-ln-20220318"
 }
+resource "aws_default_vpc" "default" {}
+
+
