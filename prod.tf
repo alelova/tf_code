66a925800acbe1c9667e01c0b2fa1b62f9e557ef
alelova <alelova@gmail.com>
2022-05-02 21:42:33 +0200

    subred

--- prod.tf
+++ prod.tf
@@ -1,12 +1,22 @@
 provider "aws" {
   profile = "default"
-  region = "us-west-2"
+  region = "eu-central-1"
 }
 
 resource "aws_s3_bucket" "prod_tf_course" {
   bucket = "tf-course-ln-20220318"
 }
 resource "aws_default_vpc" "default" {}
+
+resource "aws_subnet" "Public101" {
+  vpc_id     = aws_default_vpc.default.id
+  cidr_block = "172.31.101.0/24"
+
+  tags = {
+    Name = "Public101 Subnet"
+    "Terraform" = "true"
+  }
+}
 
 resource "aws_security_group" "prod_web"{
   name = "prod_web"
